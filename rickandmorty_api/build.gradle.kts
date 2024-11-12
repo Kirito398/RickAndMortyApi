bda7ed678d1f564e2be15be559c938ba540e1def
SIR <henkok398@mail.com>
2024-11-12 09:20:24 +0900

    jdk version update

--- rickandmorty_api/build.gradle.kts
+++ rickandmorty_api/build.gradle.kts
@@ -9,6 +9,10 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "17" // Set Kotlin target JVM to 17
+}
+
 dependencies {
     api(project(":entities:entity_api"))
     implementation(project(":rickandmorty_repository"))
--- rickandmorty_repository/build.gradle.kts
+++ rickandmorty_repository/build.gradle.kts
@@ -8,6 +8,10 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "17" // Set Kotlin target JVM to 17
+}
+
 dependencies {
     //implementation(project(":rickandmorty_api"))
     api(project(":rickandmorty_domain"))
